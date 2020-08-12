c032d4f73306c2690a97cbe6ddfd34a699f053e2
AnkleBreaker <MagallonJanjan@gmail.com>
2020-08-12 21:17:59 +0800

    second commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyCrm-lBjzBnXPgButrKAth7_hljeUTYKOY",
+    authDomain: "renzy-portfolio.firebaseapp.com",
+    databaseURL: "https://renzy-portfolio.firebaseio.com",
+    projectId: "renzy-portfolio",
+    storageBucket: "renzy-portfolio.appspot.com",
+    messagingSenderId: "63125638412",
+    appId: "1:63125638412:web:77378d38ca57ebcfe7cd29",
+    measurementId: "G-0929TWLLYP"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyCrm-lBjzBnXPgButrKAth7_hljeUTYKOY",
+    authDomain: "renzy-portfolio.firebaseapp.com",
+    databaseURL: "https://renzy-portfolio.firebaseio.com",
+    projectId: "renzy-portfolio",
+    storageBucket: "renzy-portfolio.appspot.com",
+    messagingSenderId: "63125638412",
+    appId: "1:63125638412:web:77378d38ca57ebcfe7cd29",
+    measurementId: "G-0929TWLLYP"
+  }
 };
 
 /*
