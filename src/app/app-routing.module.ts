09ab74e9184a6af52bba163f4c54d00f0196b051
AnkleBreaker <MagallonJanjan@gmail.com>
2020-08-12 20:48:00 +0800

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent },
+  {path: 'skills', component: SkillsComponent},
+  {path: 'hobbies', component: HobbiesComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path: 'contacts', component: ContactComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SkillsComponent,
+    ContactComponent,
+    ProjectsComponent,
+    HobbiesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
